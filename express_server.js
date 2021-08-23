413ff5a28ec243288a9d5374a781552e63aec989
vickyruud <aprajit.k@gmail.com>
2021-08-23 14:42:48 -0400

    adding after testing a new page /hello

--- express_server.js
+++ express_server.js
@@ -17,6 +17,10 @@ app.get('/urls.json', (req,res) => {
   res.json(urlDatabase);
 })
 
+app.get('/hello', (req, res) => {
+  res.send('<html><body> Hello <b>World</b></body></html>\n')
+})
+
 
 //log to the console that the server is listening on port 8080.
 app.listen(PORT, () => {
