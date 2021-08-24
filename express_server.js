b2b9f707d6149378b697c27fa6dd69934cca93af
vickyruud <aprajit.k@gmail.com>
2021-08-24 10:38:05 -0400

    adding urls path

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get('/hello', (req, res) => {
   res.send('<html><body> Hello <b>World</b></body></html>\n');
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 
 
 //log to the console that the server is listening on port 8080.
