d9570ad734dba096f77ea988cb38a76c4b029802
vickyruud <aprajit.k@gmail.com>
2021-08-25 09:15:59 -0400

    commit after adding post request method

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));// activating body parser for POST requests
+
 //set ejs as the engine
 app.set("view engine", "ejs");
 
@@ -25,10 +28,21 @@ app.get('/hello', (req, res) => {
   res.send('<html><body> Hello <b>World</b></body></html>\n');
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("It has been added!!");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
 
 
 
