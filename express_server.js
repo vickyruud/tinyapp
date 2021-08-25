11ab56bdd9cf68ea0b5279c418d92f76854957bd
vickyruud <aprajit.k@gmail.com>
2021-08-25 11:45:32 -0400

    commit after storing and redirecting from :shorturl

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));// activating body parser for POST requests
 
 function generateRandomString() {
-  
+
   return Math.random().toString(20).substr(2,6);
 
 }
@@ -22,7 +22,8 @@ const urlDatabase = {
 
 //on the get command send hello
 app.get('/', (req,res) => {
-  res.send("Hello!");
+  const templateVars = { urls: urlDatabase}
+  res.render("urls_index", templateVars);
 });
 
 app.get('/urls', (req,res) => {
@@ -39,14 +40,20 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("It has been added!!");
+  const shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 })
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL" , (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
 
 
 
