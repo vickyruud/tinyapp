de3111c18f90b4f4bf4dfc4c3fa2ae9229f17e6e
vickyruud <aprajit.k@gmail.com>
2021-08-27 09:18:43 -0400

    changed port as 8080 was not working

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000; //setting port
+const PORT = 5000; //setting port
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));// activating body parser for POST requests
